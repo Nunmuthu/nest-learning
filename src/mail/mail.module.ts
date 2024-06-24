f6dc355332631ec05e13ae0a1073e8577f9c1d1e
Nunmuthu <nunmuthu@gmail.com>
2024-06-24 19:01:48 +0530

    nodemailer working example

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,9 +1,38 @@
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
 import { ScheduleModule } from '@nestjs/schedule';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
 
 @Module({
   providers: [MailService],
-  imports: [ScheduleModule.forRoot()],
+  imports: [
+    ScheduleModule.forRoot(),
+    MailerModule.forRoot({
+      // transport: 'smtps://user@domain.com:pass@smtp.domain.com',
+      transport: {
+        host: 'smtp.ethereal.email',
+        port: 587,
+        // ignoreTLS: true,
+        secure: false,
+        auth: {
+          // user: process.env.MAILDEV_INCOMING_USER,
+          user: 'louvenia.baumbach@ethereal.email',
+          // pass: process.env.MAILDEV_INCOMING_PASS,
+          pass: 'XyYM1f4uDSQmqWZnZa',
+        },
+      },
+      defaults: {
+        from: '"nest-modules" <modules@nestjs.com>',
+      },
+      template: {
+        dir: __dirname + '/templates',
+        adapter: new EjsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
+  ],
 })
 export class MailModule {}
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,12 +1,38 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { Cron, CronExpression } from '@nestjs/schedule';
+// import { Cron, CronExpression } from '@nestjs/schedule';
+import { MailerService } from '@nestjs-modules/mailer';
+
 @Injectable()
 export class MailService {
+  constructor(private readonly mailerService: MailerService) {
+    this.mailexample();
+  }
+
   private readonly logger = new Logger(MailService.name);
 
-  @Cron(CronExpression.EVERY_SECOND)
+  //   @Cron(CronExpression.EVERY_SECOND)
   handleCron() {
     // this.logger.debug('Called when the current second is 45');
     console.log('Called for every second');
+  }
+
+  public mailexample(): void {
+    console.log(process.env.MAILDEV_INCOMING_USER);
+    console.log(process.env.MAILDEV_INCOMING_PASS);
+    this.mailerService
+      .sendMail({
+        from: process.env.MAILDEV_INCOMING_USER,
+        to: 'nunmuthu@gmail.com', // list of receivers
+        subject: 'Testing Nest MailerModule ✔', // Subject line
+        text: 'welcome', // plaintext body
+        html: '<b>welcome</b>', // HTML body content
+      })
+      .then(() => {
+        console.log('sent successfully');
+      })
+      .catch((error) => {
+        console.log('error successfully');
+        console.error(error);
+      });
   }
 }
