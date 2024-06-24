5b1be8754f53020c11bbdde25bddd3dc1ff8f3b3
Nunmuthu <nunmuthu@gmail.com>
2024-06-24 18:40:48 +0530

    Added cron job

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  providers: [MailService]
+  providers: [MailService],
+  imports: [ScheduleModule.forRoot()],
 })
 export class MailModule {}
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,4 +1,12 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
 @Injectable()
-export class MailService {}
+export class MailService {
+  private readonly logger = new Logger(MailService.name);
+
+  @Cron(CronExpression.EVERY_SECOND)
+  handleCron() {
+    // this.logger.debug('Called when the current second is 45');
+    console.log('Called for every second');
+  }
+}
