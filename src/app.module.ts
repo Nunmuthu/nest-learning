4e4c7df45c0643e5df01f1125b8d5f832501155b
Nunmuthu <nunmuthu@gmail.com>
2024-06-25 21:01:46 +0530

    added postgres db

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MailModule } from './mail/mail.module';
 import { EventsController } from './events/events.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Events } from './events/event.entity';
 
 @Module({
-  imports: [MailModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '127.0.0.1',
+      port: 5432,
+      username: 'postgres',
+      password: 'nunmuthu',
+      database: 'nest-events',
+      entities: [Events],
+      synchronize: true,
+    }),
+    MailModule,
+  ],
   controllers: [AppController, EventsController],
   providers: [AppService],
 })
--- src/events/event.entity.ts
+++ src/events/event.entity.ts
@@ -1,7 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Events {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column('varchar', { unique: true, length: 100 })
   name: string;
+  @Column()
   description: string;
+  @Column()
   when: Date;
+  @Column()
   address: string;
 }
